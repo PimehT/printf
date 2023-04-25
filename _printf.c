489394848dfd325e0616d08a8fcb323d958a895f
PimehT <tareerepimeh@gmail.com>
2023-04-25 04:05:32 -0700

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _printf - my own printf function
+ * @format: format specifier
+ *
+ * Return: strings like actual printf
+ */
+int _printf(const char *format, ...)
+{
+	int i, printed = 0, char_printed = 0;
+	int width, size, precision, flag, buff = 0;
+	va_list ap;
+	char buffer[BUFF_SIZE];
+
+	if (format == NULL)
+		return (-1);
+
+	va_start(ap, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff++] = format[i];
+			if (buff == BUFF_SIZE)
+				print_buffer(buffer, &buff);
+			char_printed++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff);
+			flag = get_flags(format, &i);
+			width = get_width(format, &i, ap);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, ap,
+					buffer, flag, width,
+					precision, size);
+			if (printed == -1)
+				return (-1);
+			char_printed += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff);
+
+	va_end(ap);
+
+	return(char_printed);
+}
+
+/**
+ * print_buffer - function to print buffer content
+ * @buffer: char array
+ * @buff: index of the next char, also represents length
+ * 
+ * Return: nothing
+ */
+void print_buffer(char buffer[], int *buff)
+{
+	if (buff > 0)
+		write(1, &buffer[0], *buff);
+
+	*buff = 0;
+}
