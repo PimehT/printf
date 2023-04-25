3542559bb0f10e2c4178a52f2e4c04792e6746cf
PimehT <tareerepimeh@gmail.com>
2023-04-25 08:37:30 -0700

    util.c

--- util.c
+++ util.c
@@ -52,4 +52,36 @@ int is_digit(char c)
 	return (0);
 }
 
+/**
+ * convert_size_number - casts a number to the specified size
+ * @num: number to be casted
+ * @size: tpye to be casted
+ *
+ * Return: casted value of num
+ */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
 
+	return ((int)num);
+}
+
+/**
+ * convert_size_unsgnd - casts a number to the specified size
+ * @num: number to be casted
+ * @size: type to be casted
+ *
+ * Return: casted value of num
+ */
+long int convert_size_unsgnd(unsigned long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
